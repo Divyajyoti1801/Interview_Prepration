5869097da80b8c6d1f34fb33f67bf133ec6f18e8
Divyajyoti <31211823+Divyajyoti1801@users.noreply.github.com>
2023-06-28 02:48:40 +0530

    started fresh DSA course

--- Data Structure and Algorithm/BigO.py
+++ Data Structure and Algorithm/BigO.py
@@ -1,2 +1,17 @@
 #Big 'O' notation Coding examples
 
+"""
+Big-O Asymptotic Notation
+    - it is use for comparing code1 an code2 mathematically.
+    - Comparison based on time complexity
+    - There is another comparison known as space complexity.
+    - Big O means worst case scenario
+
+"""
+
+print("Hello World")
+
+#this code will run of O(n) and have straight linear graph.
+def print_items(n):
+    for i in range(n):
+        print(i);
