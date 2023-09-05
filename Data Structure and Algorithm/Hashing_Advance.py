5cf4541b24fc3e653c99371d0f0adc4723e5dd3a
Divyajyoti <31211823+Divyajyoti1801@users.noreply.github.com>
2023-09-05 15:37:50 +0530

    Hashing_Advance completed

--- Data Structure and Algorithm/Hashing_Advance.py
+++ Data Structure and Algorithm/Hashing_Advance.py
@@ -208,8 +208,171 @@ print()
 Problem Statement: Longest subarray with equal number of zero and one
 """
 def longest_subarray_equal_zero_one_1(arr):
-    return
+    n = len(arr)
+    res = 0
+    for i in range(n):
+        c_0 = 0
+        c_1 = 1
+        for j in range(1,n):
+            if arr[j]==0:
+                c_0+=1
+            else:
+                c_1+=1
+            if c_0 == c_1:
+                res = max(res,j-i+1)
+    return res
 print("Longest subarray with equal zero and one (Naive): ",longest_subarray_equal_zero_one_1([1,0,1,1,1,0,0]))
 def longest_subarray_equal_zero_one_2(arr):
-    return
+    n = len(arr)
+    for i in range(n):
+        if arr[i] == 0:
+            arr[i] = -1 # 0 will be converted into -1 to use longest 0 sum subarray
+    res_dict = dict()
+    sum = 0
+    max_len = 0
+    for i in range(n):
+        sum  += arr[i]
+        if sum == 0:
+            max_len = i + 1
+        if sum in res_dict:
+            max_len = max(max_len,i-res_dict[sum])
+        else:
+            res_dict[sum] = i
+    return max_len
 print("Longest subarray with equal zero and one (Efficient): ",longest_subarray_equal_zero_one_2([1,0,1,1,1,0,0]))
+print()
+
+"""
+Problem Statement: Longest common span with same sum in binary Array
+"""
+def longest_common_span_binary_1(arr1,arr2):
+    res = 0
+    n = len(arr1)
+    for i in range(n):
+        sum1 = 0
+        sum2 = 0
+        for j in range(i,n):
+            sum1+=arr1[i]
+            sum2+=arr2[i]
+            if sum1 == sum2:
+                res =max(res,j-i+1)
+    return res
+print("Longest common span with same sum in Binary (Naive): ",longest_common_span_binary_1([0,1,0,0,0,0],[1,0,1,0,0,1]))
+"""
+Efficient Solution: The problem is going to reduce into the problem of longest subarray with zero sum in array
+"""
+def longest_common_span_binary_2(arr1,arr2):
+    n = len(arr1)
+    arr = [0]*n
+    for i in range(n):
+        arr[i] = arr1[i] - arr2[i] # Storing difference of elements of different arrays
+    res_dict = dict()
+    sum = 0
+    max_len = 0
+    for i in range(n): # longest subarray with zero sum
+        sum+=arr[i]
+        if sum == 0:
+            max_len = i + 1
+        if sum in res_dict:
+            max_len = max(max_len,i-res_dict[sum])
+        else:
+            res_dict[sum] = i
+    return max_len
+print("Longest common span with same sum in Binary (Efficient) (O(n)): ",longest_common_span_binary_2([0, 1, 0, 0, 0, 0],[1, 0, 1, 0, 0, 1]))
+print()
+
+"""
+Problem Statement : Longest consecutive subsequence
+"""
+def longest_consecutive_subsequence_1(arr):
+    n = len(arr)
+    arr.sort()
+    res = 1
+    curr = 1
+    for i in range(1,n):
+        if arr[i] == arr[i-1]+1:
+            curr += 1
+        else:
+            res = max(res,curr)
+            curr = 1
+    res = max(res,curr)
+    return res
+print("Longest consecutive subsequence (Naive): ",longest_consecutive_subsequence_1([1,9,3,4,2,10,13]))
+"""
+Efficient Method: 
+    - We first insert all elements in a hash table
+    - Then with 2n lookups, we find the result
+"""
+def longest_consecutive_subsequence_2(arr):
+    s = set()
+    res = 0
+    for i in arr:
+        s.add(i)
+    for i in arr:
+        if i-1 not in s:
+            curr = 1
+            while curr + i in s:
+                curr+=1
+            res = max(res,curr)
+    return res
+print("Longest consecutive subsequence (Efficient) O(n): ",longest_consecutive_subsequence_2([1,9,3,4,2,10,13]))
+print()
+
+"""
+Problem Statement: Count distinct elements in every window  
+Idea of Naive Solution:
+    - There will (n-k+1) windows
+    - Traverse through every window and count distinct elements in it.
+    - Mainly using nested loops
+Idea of Efficient Solution:
+    - The idea is to use count of previous window to get the current count
+"""
+def count_distinct_element_window_1(arr,k):
+    res = []
+    for i in range(len(arr)-k+1):
+        res.append(len(set(arr[i:i+k])))
+    return res
+print("Count distinct element in every window(Naive): ",count_distinct_element_window_1([10,20,20,10,30,40,10],4))
+def count_distinct_element_window_2(arr,k):
+    result = []
+    res = Counter(arr[0:k])
+    result.append(len(res))
+    for i in range(k,len(arr)):
+        x = arr[i-k]
+        res[x] -= 1
+        res[arr[i]] += 1
+        if res[x] == 0:
+            del res[x]
+        result.append(len(res))
+    return result
+print("Count distinct element in every window(Efficient): ",count_distinct_element_window_2([10,20,20,10,30,40,10],4))
+print()
+
+"""
+Problem Statement : More than n/k occurrences
+"""
+def more_than_n_by_k_1(arr,k):
+    res = []
+    arr.sort()
+    i = 1
+    count = 1
+    n = len(arr)
+    while i < len(arr):
+        while i<len(arr) and arr[i] == arr[i-1]:
+            count += 1
+            i+=1
+        if count > n//k:
+            res.append(arr[i-1])
+        count = 1
+        i+=1
+    return res
+print("More than n/k occurrences(Naive): ",more_than_n_by_k_1([30,10,20,20,10,20,30,30],4))
+def more_than_n_by_k_2(arr,k):
+    res = []
+    n = len(arr)
+    c = Counter(arr)
+    for x in c:
+        if c[x] > n//k:
+            res.append(x)
+    return res
+print("More than n/k occurrences (Efficient): ",more_than_n_by_k_2([30,10,20,20,10,20,30,30],4))
