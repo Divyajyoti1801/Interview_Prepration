fd698e24d607ee5c933b1dd065ae033c4eab57af
Divyajyoti <Divyajyoti182000@gmail.com>
2023-10-02 06:02:23 +0530

    GRAPH_ADVANCE : 39% Completed

--- Data Structure and Algorithm/Graph_Advance.py
+++ Data Structure and Algorithm/Graph_Advance.py
@@ -101,3 +101,106 @@ print_graph(adjacent_2)
 print("BFS Path: ")
 BFS_1(adjacent_2,0)
 print()
+
+"""
+- Version_2: BFS for Disconnected Graph
+"""
+def BFS_utility_1(adj,s,visited):
+    q = deque()
+    q.append(s)
+    visited[s] = True
+
+    while q:
+        s = q.popleft()
+        print(s,end=" ")
+
+        for u in adj[s]:
+            if visited[u] == False:
+                q.append(u)
+                visited[u] = True
+    print()
+
+def BFS_disjoint(adj):
+    visited = [False] * len(adj)
+    for u in range(len(adj)):
+        if visited[u] == False:
+            BFS_utility_1(adj,u,visited)
+
+adjacent_3 = [[1, 2], [0, 3], [0, 3], [1, 2], [5, 6], [4, 6], [4, 5]]
+print_graph(adjacent_3)
+print("BFS Path of Disjoint Graph: ")
+BFS_disjoint(adjacent_3)
+
+"""
+Connected Components in an Undirected Graph
+"""
+def BFS_utility_2(adj,s,visited):
+    q = deque()
+    q.append(s)
+    visited[s]=True
+
+    while q:
+        s = q.popleft()
+        for u in adj[s]:
+            if visited[u] == False:
+                q.append(u)
+                visited[u] = True
+    print()
+
+def BFS_disjoint_CC(adj):
+    visited = [False] * len(adj)
+    res = 0
+    for u in range(len(adj)):
+        if visited[u] == False:
+            res += 1
+            BFS_utility_2(adj,u,visited)
+    return res
+adjacent_4 = [[1, 2], [0, 2], [0, 1], [4], [3], [6,7], [5],[5]]
+print("Connected Component of a disjoint graph: ",BFS_disjoint_CC(adjacent_4))
+print()
+
+"""
+Application BFS
+    - Shortest path in an unweighted graph
+    - Web-Crawlers in Search Engine
+    - Peer-to-Peer Network
+    - Social Networking Search
+    - In Garbage Collection (Cheney's Algorithm)
+    - Cycle Detection
+    - Ford Fulkerson Algorithm
+    - Broadcasting Algorithm 
+"""
+
+"""
+Depth First Search (DFS) : Recursive in Nature
+Version_1: Choosing Min Value vertex as a starting point
+"""
+def DFS_Utility_1(adj,s,visited):
+    visited[s] = True
+    print(s,end=" ")
+    for u in adj[s]:
+        if visited[u] == False:
+            DFS_Utility_1(adj,u,visited)
+
+def DFS(adj,s):
+    visited = [False] * len(adj)
+    DFS_Utility_1(adj,s,visited)
+
+adjacent_5 = [[1, 2], [0, 3, 4], [0, 3], [1, 2, 4], [1, 3]]
+print_graph(adjacent_5)
+print("Depth Fist Search Path:")
+DFS(adjacent_5,0)
+print()
+print()
+"""
+Version 2: DFS of a Disjoint Graph
+"""
+def DFS_disjoint(adj):
+    visited = [False]*len(adj)
+    for u in range(len(adj)):
+        if visited[u] == False:
+            DFS_Utility_1(adj,u,visited)
+adjacent_6 = [[1, 2], [0, 2], [0, 1], [4], [3]]
+print_graph(adjacent_6)
+print("DFS of a Disconnected Graph: ")
+DFS_disjoint(adjacent_6)
