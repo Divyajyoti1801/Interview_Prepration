255c60eb6f0ec9f7d4ae9f5ce3aa5348cd7efe98
Divyajyoti <Divyajyoti182000@gmail.com>
2023-10-12 11:48:12 +0530

    GREEDY_ALGO - Day - 2

--- Data Structure and Algorithm/Greedy_Algorithm_Advance.py
+++ Data Structure and Algorithm/Greedy_Algorithm_Advance.py
@@ -66,4 +66,107 @@ Solution Pseudo Code:
     - Do following for remaining activities:
         = if current activity overlaps with the last picked activity in the solution ignore the current activity
         = else add the current activity to the solution
+Time Complexity: O(nlog(n))
+"""
+def max_activities(arr):
+    n = len(arr)
+    arr.sort(key = lambda x:x[1])
+    prev = 0
+    res = 1
+    for curr in range(1,n):
+        if arr[curr][0]>=arr[prev][1]:
+            res += 1
+            prev = curr
+    return res
+print("Maximum no. of activities that can happen on a single machine: ",max_activities([(12,25),(10,20),(20,30)]))
+print()
+"""
+Greedy Algo - 3 
+    - Fractional Knapsack Problem
+    Weights : 50    20    30
+    Values  : 600   500   400
+    
+    Knapsack Capacity : 70
+    Find the maximum value we can obtain in a knapsack.
+
+Pseudo Code:
+    - Calculate ratio (value/weight) for every item.
+    - Sort all item in decreasing order of the ratio.
+    - Initialize : res = 0, curr_cap = given_cap
+    - Do following for every item I in sorted order
+        = Else if (I.wight <= curr_cap)
+            {
+                curr_cap -= I.weight
+                res += I.value
+            }
+        = Else
+            {
+                res += (I.value) * (curr.cap/I.weight);
+                return res
+            }
+        = Return res
+
+Time Complexity : O(nlog(n)) 
+"""
+def fractional_knapsack(knapsack,arr):
+    n = len(arr)
+    cost_weight = []
+    
+    for i in range(n):
+        c , W = arr[i][0],arr[i][1]
+        cost_weight.append((c,W,((c*1.0)/knapsack)))
+    
+    cost_weight = sorted(cost_weight,key=lambda x:x[2],reverse=True)
+    
+    res = 0.0
+
+    for curr in cost_weight:
+        if curr[1] <= knapsack:
+            res += curr[0]
+            knapsack -= curr[1]
+        else:
+            res+=(curr[0]*(knapsack/curr[1]))
+            break
+    return res
+print("Fractional Knapsack problem : ",fractional_knapsack(50,[(120,30),(100,20),(60,10)]))
+print()
+
+"""
+Greedy Algo - 4
+    - Job Sequencing Problem
+    Deadline : 4    1   1   1
+    Profit   : 70   80  30  100 
+
+Rules:
+    - One Unit per job
+    - Only one job can be assigned at time.
+    - Time start with zero.
+
+Pseudo Code :
+    - Sort the jobs in decreasing order
+    - Add the jobs to the latest possible slot
+    - if jobs cannot be added ignore it
+"""
+def job_scheduling(arr,t):
+    n = len(arr)
+    arr.sort(key = lambda x:x[1],reverse=True)
+    result = [False] * t
+    res = 0
+
+    for i in range(n):
+        for j in range(min(t-1,arr[i][0]-1),-1,-1):
+            if result[j] == False:
+                result[j] = True
+                res += arr[i][1]
+                break
+    return res
+print("Job Sequencing Problem: ",job_scheduling([(4,50),(1,5),(1,20),(5,10),(5,80)],5))
+print()
+
+"""
+Greed Algo - 5 :
+    - Huffman Coding
+    - Used for lossless compression
+    - Variable length coding
+
 """
