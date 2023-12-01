a366e163ad2eea5a6d5a98bd4033ed48479ff890
Divyajyoti <Divyajyoti182000@gmail.com>
2023-12-01 14:14:38 +0530

    Technical_Interview : Sliding Window Day - 4

--- Technical_Interview_Concepts/Sliding_Window/Minimum_Number_Of_Flips_To_Make _Binary_String_Alternating.py
+++ Technical_Interview_Concepts/Sliding_Window/Minimum_Number_Of_Flips_To_Make _Binary_String_Alternating.py
@@ -19,7 +19,34 @@ Then, use the second operation on the third and sixth elements to make s = "1010
 
 
 def minimum_number_of_flips(s):
-    return
+    n = len(s)
+    s = s + s  # major trick
+    alt1, alt2 = "", ""
+
+    for i in range(len(s)):
+        alt1 += "0" if i % 2 else "1"
+        alt2 += "1" if i % 2 else "0"
+
+    res = len(s)
+    diff1 = 0
+    diff2 = 0
+    l = 0  # left-pointer
+
+    for r in range(len(s)):
+        if s[r] != alt1[r]:
+            diff1 += 1
+        if s[r] != alt2[r]:
+            diff2 += 1
+        if (r-l+1) > n:
+            if s[l] != alt1[l]:
+                diff1 -= 1
+            if s[l] != alt2[l]:
+                diff2 -= 1
+            l += 1
+
+        if (r-l+1) == n:
+            res = min(res, diff1, diff2)
+    return res
 
 
 print("Minimum number of flips to make Binary String Alternating: ",
